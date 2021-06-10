5fd2f01aac6d4d76537223f24aae64b88a8c9493
mayra@mumuki.org <mayra@mumuki.org>
2021-06-10 15:34:50 +0000

    Mumuki Export on 2021-06-10 15:34:50 +0000

--- 00002_Ejercicio 2/test..js
+++ 00002_Ejercicio 2/test..js
@@ -1,20 +1,20 @@
 describe("", function() {
   it("Si hacemos multiplicacion([6, 1, 34, 94, 3, 17]) retorna 977976", function() {
       let resultado = multiplicacion([6, 1, 34, 94, 3, 17]);
-      assert.equal(resultado, 155);
+      assert.equal(resultado, 977976);
   })
 });
 
 describe("", function() {
   it("Si hacemos multiplicacion([4, 8, 5, 24, 18]) retorna 59", function() {
       let resultado = multiplicacion([4, 8, 5, 24, 18]);
-      assert.equal(resultado, 59);
+      assert.equal(resultado, 69120);
   })
 });
 
 describe("", function() {
   it("Si hacemos multiplicacion([19, 27, 12, 23, 10]) retorna 91", function() {
       let resultado = multiplicacion([19, 27, 12, 23, 10]);
-      assert.equal(resultado, 91);
+      assert.equal(resultado, 1415880);
   })
 });
