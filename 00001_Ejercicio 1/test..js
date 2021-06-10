f387816fabdd35660e3b2e9d00c52bb7b2bcf156
mayra@mumuki.org <mayra@mumuki.org>
2021-06-10 15:07:29 +0000

    Mumuki Export on 2021-06-10 15:07:29 +0000

--- 00001_Ejercicio 1/test..js
+++ 00001_Ejercicio 1/test..js
@@ -1,7 +1,6 @@
 describe("", function() {
   it("Si hacemos sumatoria([6, 1, 34, 94, 3, 17]) retorna 155", function() {
-      let resultado = sumatoria([6, 1, 34, 94, 3, 17])
+      let resultado = sumatoria([6, 1, 34, 94, 3, 17]);
       assert.equal(resultado, 155);
   })
 });
-
