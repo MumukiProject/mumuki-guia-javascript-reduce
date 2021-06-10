588470c588392daf7b035bc4c1106148a86989b8
mayra@mumuki.org <mayra@mumuki.org>
2021-06-10 15:33:27 +0000

    Mumuki Export on 2021-06-10 15:33:27 +0000

--- 00002_Ejercicio 2/description.md
+++ 00002_Ejercicio 2/description.md
@@ -1,10 +1,10 @@
-* Tenemos un array llamado `numbers` con números enteros al azar.
-* Utilizando `reduce`, queremos calcular la multiplicación de todos los números que están en el array.
+Tenemos un array llamado `numbers` con números enteros al azar.
+Utilizando `reduce`, queremos calcular la multiplicación de todos los números que están en el array.
 
 ```js
-const numbers = [6, 1, 34, 94, 3, 17];
+let numeros = [6, 1, 34, 94, 3, 17];
 
-// codea debajo de este comentario la solucion al ejercicio
-
-// deberia mostrar 977976
+ム multiplicacion(numeros)
+977976
 ```
+> Definí la función `multiplicacion`.
--- 00002_Ejercicio 2/test..js
+++ 00002_Ejercicio 2/test..js
@@ -1,1 +1,20 @@
-TODO
+describe("", function() {
+  it("Si hacemos multiplicacion([6, 1, 34, 94, 3, 17]) retorna 977976", function() {
+      let resultado = multiplicacion([6, 1, 34, 94, 3, 17]);
+      assert.equal(resultado, 155);
+  })
+});
+
+describe("", function() {
+  it("Si hacemos multiplicacion([4, 8, 5, 24, 18]) retorna 59", function() {
+      let resultado = multiplicacion([4, 8, 5, 24, 18]);
+      assert.equal(resultado, 59);
+  })
+});
+
+describe("", function() {
+  it("Si hacemos multiplicacion([19, 27, 12, 23, 10]) retorna 91", function() {
+      let resultado = multiplicacion([19, 27, 12, 23, 10]);
+      assert.equal(resultado, 91);
+  })
+});
