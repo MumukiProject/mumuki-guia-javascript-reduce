396630b3888ee53c7e5b851512eb9d2ce3219d23
mayra@mumuki.org <mayra@mumuki.org>
2021-06-16 18:22:09 +0000

    Mumuki Export on 2021-06-16 18:22:09 +0000

--- 00004_Libros/test..js
+++ 00004_Libros/test..js
@@ -3,7 +3,7 @@ it(escapeHtml("Si hacemos enlistar('JavaScript for Kids: A Playful Introduction 
 'JavaScript for Kids: A Playful Introduction to Programming','Composing Software','Eloquent JavaScript: A Modern Introduction to Programming'
 ])
   assert.deepEqual(resultado, '<li>JavaScript for Kids: A Playful Introduction to Programming</li><li>Composing Software</li><li>Eloquent JavaScript: A Modern Introduction to Programming</li>'
-})
+}))
 
 it(escapeHtml("Si hacemos enlistar(['La insoportable levedad del ser','Socorro','Un lobo estepario', 'Como agua para chocolate']) retorna '<li>La insoportable levedad del ser</li><li>Socorro</li><li>Un lobo estepario</li><li>Como agua para chocolate</li>'"), function(){
   let resultado = enlistar(['La insoportable levedad del ser','Socorro','Un lobo estepario', 'Como agua para chocolate']);
@@ -12,7 +12,7 @@ it(escapeHtml("Si hacemos enlistar(['La insoportable levedad del ser','Socorro',
 it(escapeHtml("Si hacemos enlistar('Fundación','Los desposeídos', 'Ensayo sobre la ceguera') retorna '<li>Fundación</li><li>Los desposeídos</li><li>Ensayo sobre la ceguera</li>"), function(){
   let resultado = enlistar(['Fundación','Los desposeídos', 'Ensayo sobre la ceguera']);
   assert.deepEqual(resultado,'<li>Fundación</li><li>Los desposeídos</li><li>Ensayo sobre la ceguera</li>'
-})
+}))
 
 
 function escapeHtml(unsafe) {
